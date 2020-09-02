ff2f603632777498a4231ff5361c11273e6313e4
Ja Vier <javiercmh@gmail.com>
2020-09-02 02:25:28 -0400

    ssh and git config

---
+++ Linux/ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp .ssh.zip /home/javier/
+
+cd /home/javier/
+echo Type ssh password
+read pass
+unzip -P $pass .ssh.zip
+
+chmod +x permissions.sh
+./permissions.sh
+
+rm .ssh.zip permissions.sh
---
+++ config_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.email "javiercmh@gmail.com"
+git config --global user.name "Ja Vier"
+
+git config --global credential.helper store
+git pull
