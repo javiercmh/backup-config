4d56b9a8cf14eb4c7db7b75362eb2266c6a9f190
Ja Vier <javiercmh@gmail.com>
2021-02-05 15:57:37 +0100

    changes in ssh

--- Linux/ssh.sh
+++ Linux/ssh.sh
@@ -3,11 +3,15 @@
 cp ssh.zip /home/javier/
 
 cd /home/javier/
-echo Type ssh password
+echo Type ssh zip password: 
 read pass
 unzip -P $pass ssh.zip
 
-chmod +x permissions.sh
-./permissions.sh
+# set permissions
+chmod 700 .ssh
+chmod 644 .ssh/id_rsa.pub
+chmod 600 .ssh/id_rsa
+chmod 755 .ssh/known_hosts
+chmod 755 .ssh/config
 
-rm ssh.zip permissions.sh
+rm ssh.zip
